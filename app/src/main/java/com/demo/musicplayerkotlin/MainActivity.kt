8a39255ca02c16890ea182dccfbce4345e92cc6d
xin <linwx_ustc@163.com>
2023-12-01 22:32:56 +0800

    binding playlist favourite palyer

--- app/src/main/java/com/demo/musicplayerkotlin/MainActivity.kt
+++ app/src/main/java/com/demo/musicplayerkotlin/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.demo.musicplayerkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.demo.musicplayerkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_Musicplayerkotlin)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.shuffleBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, PlayerActivity::class.java))
+        }
+
+        binding.favouriteBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, FavouriteActivity::class.java))
+        }
+
+        binding.playlistBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, PlayerActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/demo/musicplayerkotlin/PlaylistActivity.kt
@@ -0,0 +1,11 @@
+package com.demo.musicplayerkotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlaylistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playlist)
+    }
+}
