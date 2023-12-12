9e298554907cd75c3d2670fe611b57381d445fc1
linwx <linwx_ustc@163.com>
2023-12-12 09:06:58 +0800

    add scan file

--- app/src/main/java/com/demo/musicplayerkotlin/MusicAdapter.kt
+++ app/src/main/java/com/demo/musicplayerkotlin/MusicAdapter.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.demo.musicplayerkotlin.databinding.MusicViewBinding
 
-class MusicAdapter(private val context: Context, private val musicList: ArrayList<String>) : RecyclerView.Adapter<MusicAdapter.MyHolder>() {
+class MusicAdapter(private val context: Context, private val musicList: ArrayList<Music>) : RecyclerView.Adapter<MusicAdapter.MyHolder>() {
     class MyHolder(binding: MusicViewBinding) : RecyclerView.ViewHolder(binding.root) {
         val title = binding.songNameMV
         val album = binding.songAlbumMV
@@ -24,6 +26,12 @@ class MusicAdapter(private val context: Context, private val musicList: ArrayLis
     }
 
     override fun onBindViewHolder(holder: MusicAdapter.MyHolder, position: Int) {
-        holder.title.text = musicList[position]
+        holder.title.text = musicList[position].title
+        holder.album.text = musicList[position].album
+        holder.duration.text = musicList[position].duration.toString()
+        Glide.with(context)
+            .load(musicList[position].artUri)
+            .apply(RequestOptions().placeholder(R.mipmap.ic_launcher_music).centerCrop())
+            .into(holder.image)
     }
 }
