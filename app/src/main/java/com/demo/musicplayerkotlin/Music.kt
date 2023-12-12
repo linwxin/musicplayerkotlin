f0d9051a8a6fca7e27e696bd8210dd4acb9e894a
linwx <linwx_ustc@163.com>
2023-12-13 00:55:34 +0800

    play first song and pause

--- app/src/main/java/com/demo/musicplayerkotlin/Music.kt
+++ app/src/main/java/com/demo/musicplayerkotlin/Music.kt
@@ -1,11 +1,25 @@
 package com.demo.musicplayerkotlin
 
-class Music(val id:String,
+import android.util.Log
+import java.util.concurrent.TimeUnit
+
+data class Music(val id:String,
             val title:String,
             val album:String,
             val artist:String,
             val duration: Long = 0,
             val path: String,
-            val artUri: String) {
+            val artUri: String)
+
+fun formatDuration(duration: Long) : String {
+    val minutes = TimeUnit.MINUTES.convert(duration, TimeUnit.MILLISECONDS)
+    var seconds = TimeUnit.SECONDS.convert(duration, TimeUnit.MINUTES) -
+            minutes * TimeUnit.SECONDS.convert(1, TimeUnit.MINUTES)
+    if (seconds >= 100) {
+        seconds = seconds.toString().substring(0, 2).toLong()
+    }
+
+
+    return String.format("%02d:%02d", minutes, seconds)
 
 }
--- app/src/main/java/com/demo/musicplayerkotlin/PlayerActivity.kt
+++ app/src/main/java/com/demo/musicplayerkotlin/PlayerActivity.kt
@@ -1,10 +1,21 @@
 package com.demo.musicplayerkotlin
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.demo.musicplayerkotlin.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
+
+    companion object {
+        lateinit var musicListPA : ArrayList<Music>
+        var songPosition: Int = 0
+        var mediaPlayer: MediaPlayer? = null
+        var isPlaying: Boolean = false
+    }
+
 
     private lateinit var binding: ActivityPlayerBinding
 
@@ -13,5 +24,66 @@ class PlayerActivity : AppCompatActivity() {
         setTheme(R.style.coolPinkNav)
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initializeLayout()
+        binding.playPauseBtnPA.setOnClickListener {
+            if (isPlaying)
+                pauseMusic()
+            else
+                playMusic()
+        }
+    }
+
+    private fun setLayout() {
+        Glide.with(this)
+            .load(musicListPA[songPosition].artUri)
+            .apply(RequestOptions().placeholder(R.mipmap.ic_launcher_music).centerCrop())
+            .into(binding.songImgPA)
+
+        binding.songNamePA.text = musicListPA[songPosition].title
+    }
+
+    private fun createMediaPlayer() {
+        try {
+            if (mediaPlayer == null)
+                mediaPlayer = MediaPlayer()
+
+            mediaPlayer!!.reset()
+            mediaPlayer!!.setDataSource(musicListPA[songPosition].path)
+            mediaPlayer!!.prepare()
+            mediaPlayer!!.start()
+            isPlaying = true
+            binding.playPauseBtnPA.setImageResource(R.drawable.pause_icon)
+
+        }  catch (e: Exception) {
+            return
+        }
+
+
+    }
+
+    private fun initializeLayout() {
+        songPosition = intent.getIntExtra("index", 0)
+        when(intent.getStringExtra("class")) {
+            "MusicAdapter" -> {
+                musicListPA = ArrayList()
+                musicListPA.addAll(MainActivity.MusicListMA)
+                setLayout()
+                createMediaPlayer()
+
+            }
+        }
+    }
+
+    private fun playMusic() {
+        binding.playPauseBtnPA.setImageResource(R.drawable.pause_icon)
+        isPlaying = true
+        mediaPlayer!!.start()
+    }
+
+    private fun pauseMusic() {
+        binding.playPauseBtnPA.setImageResource(R.drawable.play_icon)
+        isPlaying = false
+        mediaPlayer!!.pause()
     }
 }
+
